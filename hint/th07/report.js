f4ede1341bc5a01138ab79262dd1744fe5c786c8
Yukino Ikegami <yknikgm@gmail.com>
2016-03-28 02:10:12 +0900

    Add 東方妖々夢

---
+++ hint/th07/report.js
@@ -0,0 +1,5 @@
+document.write("<img src='http://seirei.ath.cx/rain/access/access/report.cgi?");
+document.write("dir=touhou","&amp;");
+document.write("pix=",screen.width,"x",screen.height,"&amp;");
+document.write("ref=",document.referrer);
+document.write("' width='1' height='1' alt=''>");
